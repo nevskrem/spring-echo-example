f831d73d1d0f9fd3c7a5807fd3df0981b3a7876d
Sven Merk <33895725+nevskrem@users.noreply.github.com>
2019-03-06 10:02:05 +0100

    Delete Central Build.groovy

--- .pipeline/extensions/Central Build.groovy
+++
@@ -1,8 +0,0 @@
-void call(Map params) {
-    //access stage name
-    echo "Start - Extension for stage: ${params.stageName}"
-    sh "mvn clean package"
-    stash name: 'buildArtifacts', includes: './target/**/*'
-    echo "End - Extension for stage: ${params.stageName}"
-}
-return this
