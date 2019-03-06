335aa84d1a484ba93a3385db7d759b54ec3c6b99
Sven Merk <33895725+nevskrem@users.noreply.github.com>
2019-03-06 11:08:40 +0100

    Update Acceptance.groovy

--- .pipeline/extensions/Acceptance.groovy
+++ .pipeline/extensions/Acceptance.groovy
@@ -2,6 +2,7 @@ void call(Map params) {
     //access stage name
     echo "Start - Extension for stage: ${params.stageName}"
     unstash name: 'buildArtifacts'
+    params.script.globalPipelineEnvironment.configuration.runStep.Acceptance.downloadArtifactsFromNexus = false
     //execute original stage as defined in the template
     params.originalStage()
     echo "End - Extension for stage: ${params.stageName}"
