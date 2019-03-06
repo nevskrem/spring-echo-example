3642be38621c56139d27b6bb67f76c5b45de968d
Sven Merk <33895725+nevskrem@users.noreply.github.com>
2019-03-06 10:54:05 +0100

    Update Central Build.groovy

--- .pipeline/extensions/Central Build.groovy
+++ .pipeline/extensions/Central Build.groovy
@@ -3,7 +3,7 @@ void call(Map params) {
     echo "Start - Extension for stage: ${params.stageName}"
     sh "mvn clean package"
     
-    sh "mv .target/spring-echo-example-${params.script.globalPipelineEnvironment.getArtifactVersion()}.jar ./spring-echo-example.jar"
+    sh "mv ./target/spring-echo-example-${params.script.globalPipelineEnvironment.getArtifactVersion()}.jar ./spring-echo-example.jar"
     stash name: 'buildArtifacts', includes: './spring-echo-example.jar'
     echo "End - Extension for stage: ${params.stageName}"
 }
