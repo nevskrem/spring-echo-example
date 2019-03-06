5266310481bef656c1cac4384e6fec83fe955ef3
Sven Merk <33895725+nevskrem@users.noreply.github.com>
2019-03-06 10:07:47 +0100

    Update Acceptance.groovy

--- .pipeline/extensions/Acceptance.groovy
+++ .pipeline/extensions/Acceptance.groovy
@@ -1,7 +1,6 @@
 void call(Map params) {
     //access stage name
     echo "Start - Extension for stage: ${params.stageName}"
-    sh "mvn clean package"
     //execute original stage as defined in the template
     params.originalStage()
     echo "End - Extension for stage: ${params.stageName}"
