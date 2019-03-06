d1f9f3a978c7a58fe2a20b568b95ce46e9308619
Sven Merk <33895725+nevskrem@users.noreply.github.com>
2019-03-06 09:40:02 +0100

    Update Central Build.groovy

--- .pipeline/extensions/Central Build.groovy
+++ .pipeline/extensions/Central Build.groovy
@@ -1,12 +1,8 @@
 void call(Map params) {
     //access stage name
     echo "Start - Extension for stage: ${params.stageName}"
-
-    //access config
-    echo "Current stage config: ${params.config}"
     sh "mvn clean package"
     stash name: 'buildArtifacts', includes: './target/**/*'
-
     echo "End - Extension for stage: ${params.stageName}"
 }
 return this
